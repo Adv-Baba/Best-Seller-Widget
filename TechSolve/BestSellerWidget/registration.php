de845fed188bb4cf1981430f77e128affc0126ba
Rakesh <rdebnath@mcfadyen.com>
2020-05-15 03:01:42 +0530

    magento configuration is chnaged

--- TechSolve/BestSellerWidget/registration.php
+++ TechSolve/BestSellerWidget/registration.php
@@ -1,7 +1,7 @@
 <?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mcfadyen_BestSellerWidget',
-    __DIR__
-);
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'TechSolve_BestSellerWidget',
+	__DIR__
+	);
